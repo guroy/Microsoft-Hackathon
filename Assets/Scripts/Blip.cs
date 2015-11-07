8c5bb7ec3da79b4b1aaa80d659722e988ceab5eb
unknown <bmp7101@GDD-39.main.ad.rit.edu>
2015-11-07 15:04:47 -0500

    mini map v.01

---
+++ Assets/Scripts/Blip.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blip : MonoBehaviour {
+	public Transform Target;
+	public bool KeepInBounds = false;
+	public bool LockScale = false;
+	public bool LockRotation = false;
+	public float MinScale = 1f;
+
+	MiniMap map;
+	RectTransform myRectTransform;
+
+	void Start(){
+		map = GetComponentInParent<MiniMap>();
+		myRectTransform = GetComponent<RectTransform> ();
+	}
+
+	void LateUpdate(){
+		Vector2 newPosition = map.TransformPosition (Target.position);
+
+		if (KeepInBounds)
+			newPosition = map.MoveInside (newPosition);
+
+		if (!LockScale) {
+			float scale = Mathf.Max(MinScale, map.ZoomLevel);
+			myRectTransform.localScale = new Vector3 (scale, scale, 1);
+		}
+
+		if (!LockRotation)
+			myRectTransform.localEulerAngles = map.TransformRotation (Target.eulerAngles);
+
+		myRectTransform.localPosition = newPosition;
+	}
+}
