c0fe40e387dd667de28e5b010577498198de1310
unknown <IGMAdmin@GDD-35.main.ad.rit.edu>
2015-11-06 22:26:19 -0500

    WIP

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+    public delegate void MouseDragAction();
+    public static event MouseDragAction OnMouseDrag;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Click"))
+        {
+            if (OnMouseDrag != null)
+                OnMouseDrag();
+        }
+    }
+}
