301b5d6be4ca42fcf25ffa66455eaac2b1036298
unknown <lxk7945@GDD-36.main.ad.rit.edu>
2015-11-07 09:40:47 -0500

    now with money from destroying

---
+++ Assets/Scripts/UI/hud.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class hud : MonoBehaviour {
+
+	public GameObject playerShip;
+	public Text hull;
+	public Text money;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		money.text = playerShip.GetComponent<playerShip>().money.ToString();
+		hull.text = "Hull: " + playerShip.GetComponent<playerShip>().hull.ToString();
+	
+	}
+}
